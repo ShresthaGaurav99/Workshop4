18edd24f8e88ed4cdaeabde2f42aaea6fafc7beb
shres <shres@GAURAV_1>
2025-11-19 09:25:07 +0545

    Task1 completed

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating first object
+        Student s1 = new Student();
+        s1.Name = "Gaurav";
+        s1.Age = 21;
+        s1.Address = "Dharan";
+
+        // Creating second object
+        Student s2 = new Student();
+        s2.Name = "Dev";
+        s2.Age = 21;
+        s2.Address = "Ithari";
+
+        // Display values of first object
+        Console.WriteLine("Student 1 Details:");
+        Console.WriteLine("Name: " + s1.Name);
+        Console.WriteLine("Age: " + s1.Age);
+        Console.WriteLine("Address: " + s1.Address);
+
+        Console.WriteLine();
+
+        // Display values of second object
+        Console.WriteLine("Student 2 Details:");
+        Console.WriteLine("Name: " + s2.Name);
+        Console.WriteLine("Age: " + s2.Age);
+        Console.WriteLine("Address: " + s2.Address);
+
+        Console.WriteLine();
+
+        // Display static field
+        Console.WriteLine("College Name: " + Student.CollegeName);
+    }
+}
---
+++ Workshop4/Student.cs
@@ -0,0 +1,10 @@
+﻿public class Student
+{
+    // Instance fields (unique for each object)
+    public string Name;
+    public int Age;
+    public string Address;
+
+    // Static field (shared by all objects)
+    public static string CollegeName = "IIC College";
+}
